0c8d0aa0cf9363faf0a7b2f8ce62ebbf56c0e2ec
candyP <2689825705@qq.com>
2025-12-13 14:55:15 +0800

    refactor:Deprecated

--- src/main/java/com/example/cooking/controller/SessionController.java
+++ src/main/java/com/example/cooking/controller/SessionController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Deprecated
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
